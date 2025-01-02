7a358a556df5dcf4d19e82856cb2fa0aab13e63e
nicklaswright <139339798+nicklaswright@users.noreply.github.com>
2025-01-02 11:38:13 +0100

    Update resolution_ctrl.vhdl

--- src/resolution_ctrl.vhdl
+++ src/resolution_ctrl.vhdl
@@ -3,7 +3,7 @@
 -- Verification : Nicklas Wright
 -- Reviewers    : Galo Sanchez, Hampus Lang, Simon Jansson
 -- Module       : resolution_ctrl.vhdl
--- Parent       : receiver.vhdl
+-- Parent       : awg.vhdl
 -- Children     : none
 
 -- Description: Resolution control module. This module assumes the waveform to be of 14-bit signed representation.
